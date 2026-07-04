a37c5b274f4104fb39b0188792b46766d977685a
GawainsGreenGirdle <n1ghtmar3map@gmail.com>
2026-07-04 19:45:05 -0400

    Remove 'bool' and minor bugs in c codebase
    The code could not be run because of a number of errors. This commit
    replaces all instances of "bool" with "int", "true" with 1, and "false"
    with 0. It further adds the <stdlib.h> library.

--- appOutline.c
+++ appOutline.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <sqlite3.h>
+#include <stdlib.h>
 
 //global variables
 
@@ -16,52 +16,54 @@ int numInputs = 2;
 //input array
 char *inputs[2] = {close, help};
 
-//The parse method compares the input to a list of possible inputs, 
-//calling the corresponding function. 
-//If the input is invalid, it prints 'invalid input'
-void parse(char *str){
-	int i = 0;
-	while(i < numInputs && equalsStr(str, inputs[i]) != true){
-		i++;
-	}
-	switch(i){
-		case 0: help();
-		case 1: close();
-		default: printf("No matching input");
-	}
-}
-
 //help input
-void help(){
-	printf("You have reached the help function, please press 'c' to end the program");
+void appHelp(){
+	printf("You have reached the help function, please press 'c' to end the program\n");
 }
 
 //close input
-void close(){
-	printf("closing the program");
+void appClose(){
+	printf("closing the program\n");
 	exit(0);
 }
 
 //equalsStr is used to compare two strings, a and b
 //It returns true if a and b are identical and false otherwise. 
 //It is case sensitive.
-bool equalsStr(char *a, char *b){
+int equalsStr(char *a, char *b){
 	while(*a == *b){
 		if(*a == '\0'){
-			return true;
+			return 1;
 		}
 	}
-	return false;
+	return 0;
+}
+
+//The parse method compares the input to a list of possible inputs,
+//calling the corresponding function.
+//If the input is invalid, it prints 'invalid input'
+void parse(char *str){
+        int i = 0;
+        while(i < numInputs && equalsStr(str, inputs[i]) != 1){
+                i++;
+        }
+        switch(i){
+                case 0: appHelp();
+                case 1: appClose();
+                default: printf("No matching input\n");
+        }
 }
 
 //main method
 int main(){
-	printf("Enter 'help' for a list of available commands");
-	bool cont = true;
+	printf("Enter 'help' for a list of available commands.\n");
+	int cont = 1;
 	while(cont){
-		print("Failnaught: ");
+		printf("Failnaught: ");
 		char *p = fgets(operation, 150, stdin);
 
-		parse(operation);	
+		parse(operation);
+		
+		cont = 0;	
 	}
 }
